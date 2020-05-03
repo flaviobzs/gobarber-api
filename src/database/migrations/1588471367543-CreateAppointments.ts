ef5e50b521b7d879590f32782f7862f6a11c036f
FlavioBS <bzsflavio@gmail.com>
2020-05-03 01:48:22 -0300

    Relacionamento nos models

--- src/database/migrations/1588471367543-CreateAppointments.ts
+++ src/database/migrations/1588471367543-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1588471367543
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
             generationStrategy: "uuid",
             default: "uuid_generate_v4()",
