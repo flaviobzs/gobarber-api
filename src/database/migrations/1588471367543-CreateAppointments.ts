7799ad6fc2cd1cddc3c91d724f6a95b4d859d603
FlavioBS <bzsflavio@gmail.com>
2020-05-03 01:08:21 -0300

    Repository do typeORM

--- src/database/migrations/1588471367543-CreateAppointments.ts
+++ src/database/migrations/1588471367543-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1588471367543
             type: "varchar",
             isPrimary: true,
             generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
           },
           {
             name: "provider",
