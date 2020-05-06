5d20aa0b567a903de0dd78f57e6fad34415e3a47
FlavioBS <bzsflavio@gmail.com>
2020-05-06 10:48:36 -0300

    Provider de storage

--- src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
+++ src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -3,11 +3,11 @@ import { hash, compare } from "bcryptjs";
 import IHashProvider from "../models/IHashProvider";
 
 export default class BCryptHashProvider implements IHashProvider {
-  generateHash(payload: string): Promise<string> {
+  public async generateHash(payload: string): Promise<string> {
     return hash(payload, 8);
   }
 
-  compareHash(payload: string, hashed: string): Promise<boolean> {
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
     return compare(payload, hashed);
   }
 }
