5ab7cd21fb0945087a10e9cb313908ca8189e9c2
FlavioBS <bzsflavio@gmail.com>
2020-05-06 09:18:27 -0300

    testando autenticação / criando provider de cripto

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,15 +2,20 @@ import AppError from "@shared/errors/AppError";
 
 import FakeUsersRepository from "../repositories/fakes/FakeUsersRepository";
 import FakeHashProvider from "../providers/HashProvider/fakes/FakeHashProvider";
-
+import AuthenticateUserService from "./AuthenticateUserService";
 import CreateUserService from "./CreateUserService";
 
-describe("CreateUser", () => {
-  it("should be able to create a new user", async () => {
+describe("AuthenticateUser", () => {
+  it("should be able to authenticate", async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashRepository = new FakeHashProvider();
 
     const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashRepository,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
       fakeUsersRepository,
       fakeHashRepository,
     );
@@ -21,30 +26,12 @@ describe("CreateUser", () => {
       password: "123456",
     });
 
-    expect(user).toHaveProperty("id");
-  });
-
-  it("should not be able to create a new user same email from another", async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashRepository = new FakeHashProvider();
-
-    const createUser = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashRepository,
-    );
-
-    await createUser.execute({
-      name: "John Doe",
+    const response = await authenticateUser.execute({
       email: "johndoe@example.com",
       password: "123456",
     });
 
-    expect(
-      createUser.execute({
-        name: "John Doe",
-        email: "johndoe@example.com",
-        password: "123456",
-      }),
-    ).rejects.toBeInstanceOf(AppError);
+    expect(response).toHaveProperty("token");
+    expect(response.user).toEqual(user);
   });
 });
